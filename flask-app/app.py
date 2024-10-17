3e68efc90fdd09025fdb2bf66f0f8260cf3fb636
atharvajog <atharvajog2002@gmail.com>
2024-10-17 17:02:56 +0530

    updated code

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,8 +1,12 @@
-from flask import Flask
 import os
+from flask import Flask
 app = Flask(__name__)
  
 @app.route("/")
 def hello():
-    env_variable_value = os.getenv('ENV_VAR', 200)
-    return f"<html><body><h1>Hello, This is Flask Application! Environmenrt Variable value = {env_variable_value}</h1></body></html>\n"
+    return f"<html><body><h1>Flask Deployment Successful for CI/CD checking</h1></body></html>\n"
+ 
+@app.route("/env")
+def show_env():
+    env_var = os.getenv('ENV_VARIABLE',None)
+    return f"<html><body><h1>Environment Variable: {env_var}</h1></body></html>\n"
